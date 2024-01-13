b5aa167bd6eba2ed14274f4b9a623a5ef1197701
Kartikey Jaiswal <63246079+karan74jaiswal@users.noreply.github.com>
2024-01-13 06:52:26 +0530

    Update Message.jsx

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,7 +3,7 @@ export function Message() {
     <section className="message show">
       <img src="midyAI.png" alt="midyAI logo" />
       <h1 className="text-4xl sm:text-[52px] font-bold font-secondary text-gray-800 dark:text-gray-100 uppercase relative">
-        MIDYAI SDXL
+        MIDYAI REPLACE BACKGROUND
       </h1>
       <h2 className="text-xl font-semibold text-gray-600 dark:text-gray-400">
         Teleport anything, anywhere with AI
