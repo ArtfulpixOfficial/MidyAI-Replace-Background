28a255419b8011511c168851403b11cf000515b4
ArtfulpixOfficial <143751560+ArtfulpixOfficial@users.noreply.github.com>
2024-01-23 02:19:48 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: "50mb",
+  }));
 
 app.get("/", (req, res) => {
   res.send("server is running");
