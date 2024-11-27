1bfc82fe960fef75b14ea586c963f5eede32479a
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-11-28 03:13:56 +0530

    changes doen acoording to presentaion

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,12 @@
 export function Message() {
   return (
     <section className="message show">
-      <img src="logo_dark.png" alt="midyAI logo" />
+      <img src="logo.png" alt="midyAI logo" />
       <h1 className="text-4xl sm:text-[52px] font-bold font-secondary text-gray-800 dark:text-gray-100 uppercase relative">
         MIDYAI REPLACE BACKGROUND
       </h1>
       <h2 className="text-xl font-semibold text-gray-600 dark:text-gray-400">
-        Move anything effortlessly across space with AI
+        Seamlessly swap backgrounds in seconds with ease
       </h2>
     </section>
   );
